db942042cfb1b687b51da5af175187a3a1f7606c
Ichigo0125 <152695829+Ichigo0125@users.noreply.github.com>
2026-05-18 07:40:08 +0000

    ci fail test

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,32 @@
-import Fastify, { FastifyServerOptions } from 'fastify';
 
-export function buildApp(options: FastifyServerOptions = {}) {
-  const app = Fastify({
-    logger: options.logger ?? true,
-    ...options
+import Fastify,
+{FastifyServerOptions}
+from 'fastify';
+
+export function buildApp(
+options:FastifyServerOptions= {}
+)
+{
+  const app=Fastify({
+logger:options.logger??true,
+...options
   });
 
-  app.get('/', async () => {
+
+  app.get('/',async()=>{
     return {
-      message: 'CI/CD Lab Fastify app is running',
-      version: process.env.APP_VERSION || 'dev'
-    };
+      message:'CI/CD Lab Fastify app is running',
+      version:process.env.APP_VERSION||'dev'
+    }
   });
 
-  app.get('/health', async () => {
+
+  app.get('/health',async()=>{
     return {
-      status: 'ok'
-    };
+      status:'ok'
+    }
   });
+
 
   return app;
 }
